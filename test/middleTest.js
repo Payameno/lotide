9e58f5d624079c251bdbe98f544cffa58d354ebd
Payam Beigi <beigi.payam@gmail.com>
2022-06-06 22:07:43 +0000

    adding M&C testing to middle test file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,28 @@
-const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require("chai").assert;
+const middle = require("../middle");
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#Middle: Finds the middle number in an array", () => {
+  it("Should return an empty array given an array with one item", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("Should return an empty array given an array with two items", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("Should return [2] given [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("Should return [3] given [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("Should return [2, 3] given [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("Should return [3, 4] given [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
