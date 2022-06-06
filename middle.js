ffc8ccc19dce29caaf6a08d8c792f8238131d77c
Payam Beigi <beigi.payam@gmail.com>
2022-06-06 20:57:07 +0000

    removed assertArrayEqual and equalArrays from this file, also fixed a bug with the line 6, where the output would be a number not an array in case of an odd, 3 elemnt array input

--- middle.js
+++ middle.js
@@ -1,28 +1,13 @@
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    } 
-    }
-    return true;
-}
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (!eqarrays(arr1, arr2)) {
-    console.log("🚫Arrays are not equal🚫");
-  } else {
-    console.log("🟢Arrays are equal🟢");
-  }
-};
-
 const middle = function(baseArr) {
   let middleNum = [];
   if (baseArr.length === 1 || baseArr.length === 2) {
     return [];
   } else if (baseArr.length % 2 !== 0) {
-    middleNum = ((baseArr.length + 1) / 2);
+    middleNum = [((baseArr.length + 1) / 2)];
     } else if (baseArr.length % 2 === 0){
       middleNum = [(baseArr.length / 2), ((baseArr.length + 2) / 2)];
     }
     return middleNum;
 };
+
+module.exports = middle;
