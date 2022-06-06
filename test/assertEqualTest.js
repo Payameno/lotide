4e43f79010a4aae1c16839dd667c005989726d27
Payam Beigi <beigi.payam@gmail.com>
2022-06-06 17:14:18 +0000

    test file for lotide assertEqual.js, modules linked and tested

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("test", "Test");
+assertEqual("Test", "Test");
+assertEqual(15, 15.1);
